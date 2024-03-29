022900561839c2496865265dddf7a19d1e11783a
Nikiforos Ioannidis <nioannidis@outlook.com>
2024-03-29 19:13:51 +0200

    component input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,20 @@ import { PersonTableComponent } from './components/person-table/person-table.com
 })
 export class AppComponent {
   name = 'Nikiforos';
+
+  person0 =  {
+    givenName: "Nikiforos",
+    surName: "Ioannidis",
+    age: 36,
+    email: "nioannidis@aueb.gr",
+    address: "Athens, Greece"
+  };
+
+  person1 =  {
+    givenName: "John",
+    surName: "Doe",
+    age: 0x25,
+    email: "john@aueb.gr",
+    address: "New York, USA"
+  };
 }
