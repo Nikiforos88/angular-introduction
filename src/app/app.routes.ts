ebfc316ff0725597140e64192e6abafa39b27846
Nikiforos Ioannidis <nioannidis@outlook.com>
2024-04-01 21:24:49 +0300

    simple datatable primer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { EventBindExampleComponent } from './components/event-bind-example/event
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { ComponentInputExampleComponent } from './components/component-input-example/component-input-example.component';
 import { ForDerictiveExampleComponent } from './components/for-derictive-example/for-derictive-example.component';
+import { SimpleDatatableExampleComponent } from './components/simple-datatable-example/simple-datatable-example.component';
 
 export const routes: Routes = [
     {path: 'event-bind-example', component:EventBindExampleComponent},
     {path: 'component-input-example', component:ComponentInputExampleComponent},
     {path: 'for-directive-example', component:ForDerictiveExampleComponent},
+    {path: 'simple-datatable-example', component:SimpleDatatableExampleComponent},
     {path: '', component: WelcomeComponent}
 ];
